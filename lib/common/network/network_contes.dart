1dd1b40a70751d563ab5f1ffc98b54da94400ba1
selena sameh <selenasameh31-ux>
2025-11-23 14:08:00 +0200

    api_done

---
+++ lib/common/network/network_contes.dart
@@ -0,0 +1,7 @@
+class NetworkContes {
+  static const String baseUrl = 'https://newsapi.org';
+  static const String sourcesEndPoint =
+      '/v2/top-headlines/sources'; //apikey,category
+  static const String articlesEndPoint = '/v2/everything'; //apikey,sources
+  static const String apiKey = '4f6b7ee3563d42b59f8308c2ff1dfb0e';
+}
--- lib/pages/views/category_details_view.dart
+++ lib/pages/views/category_details_view.dart
@@ -1,63 +1,76 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/pages/widgets/news_card_widget.dart';
+import 'package:news_app/common/network/apis_service.dart';
+import 'package:news_app/models/articles_model.dart';
+import 'package:news_app/models/source_mode.dart';
+import 'package:news_app/pages/widgets/article_card_widget.dart';
+import 'package:news_app/providers/category_provider.dart';
+import 'package:provider/provider.dart';
 
 class CategoryDetailsView extends StatelessWidget {
   const CategoryDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      child: Column(
-        children: [
-          TabBar(
-            indicatorSize: TabBarIndicatorSize.tab,
-            dividerColor: Colors.transparent,
-            isScrollable: true,
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            // indicatorPadding: EdgeInsetsGeometry.all(0),
-            tabAlignment: TabAlignment.start,
-            tabs: [
-              Tab(text: 'first'),
-              Tab(text: 'sec'),
-              Tab(text: 'sec'),
-              Tab(text: 'sec'),
-              Tab(text: 'sec'),
+    CategoryProvider provider = Provider.of<CategoryProvider>(context);
+    return FutureBuilder<SourceModel?>(
+      future: ApisService.getSources(provider.selectedCategory!.name),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (asyncSnapshot.hasError) {
+          return Text(asyncSnapshot.error.toString());
+        }
+        List<Sources> sources = asyncSnapshot.data?.sources ?? [];
+        return DefaultTabController(
+          length: sources.length,
+          child: Column(
+            children: [
+              TabBar(
+                indicatorSize: TabBarIndicatorSize.tab,
+                dividerColor: Colors.transparent,
+                isScrollable: true,
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                // indicatorPadding: EdgeInsetsGeometry.all(0),
+                tabAlignment: TabAlignment.start,
+                tabs: sources.map((e) => Tab(text: e.name ?? '')).toList(),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: sources
+                      .map((e) => ArticlesList(sourceID: e.id!))
+                      .toList(),
+                ),
+              ),
             ],
           ),
-          Expanded(
-            child: TabBarView(
-              children: [
-                ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemBuilder: (context, index) => NewsCardWidget(),
-                  itemCount: 5,
-                ),
-                ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemBuilder: (context, index) => NewsCardWidget(),
-                  itemCount: 5,
-                ),
-                ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemBuilder: (context, index) => NewsCardWidget(),
-                  itemCount: 5,
-                ),
-                ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemBuilder: (context, index) => NewsCardWidget(),
-                  itemCount: 5,
-                ),
-                ListView.builder(
-                  padding: EdgeInsets.all(16),
-                  itemBuilder: (context, index) => NewsCardWidget(),
-                  itemCount: 5,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+        );
+      },
+    );
+  }
+}
+
+class ArticlesList extends StatelessWidget {
+  const ArticlesList({super.key, required this.sourceID});
+  final String sourceID;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ArticlesModel>(
+      future: ApisService.getArticles(sourceID),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (asyncSnapshot.hasError) {
+          return Center(child: Text(asyncSnapshot.error.toString()));
+        }
+        ArticlesModel articlesModel = asyncSnapshot.data!;
+        List<Articles> articles = articlesModel.articles ?? [];
+
+        return ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (context, index) =>
+              ArticleCardWidget(articles: articles[index]),
+        );
+      },
     );
   }
 }
